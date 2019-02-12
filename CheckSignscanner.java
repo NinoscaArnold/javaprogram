f49f1c5e7ef88d666da63c91da9e8f4586fd8eae
NinoscaArnold <47312496+NinoscaArnold@users.noreply.github.com>
2019-02-12 09:08:21 +0530

    Create CheckSignscanner.java

---
+++ CheckSignscanner.java
@@ -0,0 +1,17 @@
+import java.util.scanner;
+class CheckSign{
+public static void main(String[] qwe){
+Scanner scan=new Scanner(System.in);
+inta=scan.nextInt();
+
+if(a<0) {
+System.out.println("Negative");
+}
+else if(a==0) {
+System.out.println("zero");
+}
+else {
+System.out.println("positive");
+}
+}
+}
