258db1e022b707b98cf73f013cd58daff3700577
NinoscaArnold <47312496+NinoscaArnold@users.noreply.github.com>
2019-02-14 09:57:25 +0530

    Create PowerOfExponentK.java

---
+++ PowerOfExponentK.java
@@ -0,0 +1,14 @@
+import java.util.scxanner;
+class PowerOfExponentK{
+public static void main(String[] qwe)
+Scanner scan=new Scanner(System.in);
+int N=scan.nextInt();
+int K=scan.nextInt();
+int res=1;
+int i=1;
+while (i<=k){
+res=res*N;
+i++;
+}
+System.out.println("N^kis:"+res);
+}
